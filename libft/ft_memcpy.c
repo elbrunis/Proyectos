61fc28423ac55fcd231413595929d5da76ff031c
elbrunis <brunoiniestabarrera@gmail.com>
2024-09-18 18:48:33 +0200

    Libft
    Primeros proyectos

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biniesta <biniesta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 12:10:08 by biniesta          #+#    #+#             */
+/*   Updated: 2024/09/18 15:43:57 by biniesta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
+
+int	main(void)
+{
+	char src[] = "\0";
+	char dest[12] = "\0";
+	ft_memcpy(dest, src, 7); 
+	memcpy(dest, src, 7);
+	printf("%s\n", dest);
+	return (0);
+}
---
+++ libft/libft.h
@@ -0,0 +1,13 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+
+int		ft_isalnum(char c);
+int		ft_strlen(char *str);
+void	*memset(void *ptr, int value, size_t num);
+
+#endif
