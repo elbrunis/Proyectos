d1449f16e9066f33cce1632f869a2e9d2f9dfcc1
Bruno Iniesta <biniesta@c2r7s5.42madrid.com>
2024-12-12 19:31:28 +0100

    primer intento

---
+++ get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biniesta <biniesta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 13:53:55 by biniesta          #+#    #+#             */
+/*   Updated: 2024/12/09 21:25:23 by biniesta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char				*free_and_join(char *save, char *read);
+char				*create_line(char *save);
+char				*put_rest(char *save);
+char				*read_file(int fd, char *save);
+char				*get_next_line(int fd);
+
+char				*ft_strchar(char *str, char c);
+void				ft_bzero(void *str, unsigned long int n);
+unsigned long int	ft_strlen(char *str);
+void				*ft_calloc(unsigned long int nmemb, unsigned long int size);
+char				*ft_strjoin(char *s1, char *s2);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biniesta <biniesta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 12:58:43 by biniesta          #+#    #+#             */
+/*   Updated: 2024/12/09 21:03:25 by biniesta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+unsigned long int	ft_strlen(char *str)
+{
+	unsigned long int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchar(char *str, char c)
+{
+	unsigned long int	i;
+
+	i = 0;
+	while (str[i] != c && str[i] != '\0')
+		i++;
+	if (str[i] == c)
+		return (&str[i]);
+	return (NULL);
+}
+
+void	ft_bzero(void *str, unsigned long int n)
+{
+	char				*s;
+	unsigned long int	i;
+
+	s = (char *)str;
+	i = 0;
+	while (i < n)
+		s[i++] = '\0';
+}
+
+void	*ft_calloc(unsigned long int nmemb, unsigned long int size)
+{
+	char	*memory;
+
+	memory = malloc(nmemb * size);
+	if (!memory)
+		return (NULL);
+	ft_bzero(memory, (nmemb * size));
+	return (memory);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char				*new;
+	unsigned long int	i;
+	size_t				s1len;
+	size_t				s2len;
+
+	if (!s1 && !s2)
+		return (NULL);
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	new = calloc((s1len + s2len + 1), sizeof(char));
+	if (!new || !s1 || !s2)
+		return (NULL);
+	i = -1;
+	while (++i < s1len)
+		new[i] = s1[i];
+	i = -1;
+	while (++i < s2len)
+		new[s1len + i] = s2[i];
+	new[s1len + i] = '\0';
+	return (new);
+}
